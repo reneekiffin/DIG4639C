a84656f1fbd504d58bf323d35b5b16c08ea6fdda
Renee <reneekiffin@outlook.com>
2019-01-25 13:46:12 -0500

    Adjustments

--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -1,9 +1,8 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: ''}//, nameAvailable()};//missing code
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -11,14 +10,30 @@ class NameForm extends React.Component {
 
   handleChange(event) {
     this.setState({value: event.target.value});
+    this.name = event.target.value;
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+    //alert('A name was submitted: ' + this.state.value);
+    //event.preventDefault();
+
+    if (/[a-zA-Z]+/.text(this.state.value)) {
+      this.setState({nameAvailable:true});
+    }
+    else{
+      alert('This input is not a name');
+      this.setState({isValid:true})
+    };
+
+  event.preventDefaultfault();
+
   }
 
   render() {
+    //let returnArray =[]//missing code
+
+    if(!this.state.nameAvailable){
+
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -26,9 +41,18 @@ class NameForm extends React.Component {
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
+
       </form>
     );
+  } else {
+    return (<div>Good Morning{this.state.value}</div>);
+  }
+
+    if (!this.state.isValid){
+      return (<p>Must be a valid a-Z or A-Z</p>);}
+
+
   }
 }
 
- export default NameForm;
+ export default NameForm;//name of class you want to export
