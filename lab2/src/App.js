31c828ef17a790887c484f4bf88099aba0de8bf7
Renee <reneekiffin@outlook.com>
2019-01-29 21:01:49 -0500

    Expo

---
+++ lab2/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NameForm from './NameForm';
+class App extends Component {
+
+  render() {
+    return (
+<NameForm/>
+);
+}
+}
+
+export default App;
---
+++ lab2/src/NameForm.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}//, nameAvailable()};//missing code
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.name = event.target.value;
+  }
+
+  handleSubmit(event) {
+    alert('Name submitted: ' + this.state.value);
+    event.preventDefault();
+
+    if (this.state.value.match (/^[a-zA-Z]+$/)) {
+      this.setState({nameAvailable:true});
+    }
+    else {
+
+
+      document.write("<span style=\"color:red;\">Name must include letters only<\/span>");
+      this.setState({isValid:true})
+    };
+
+
+
+
+  event.preventDefault();
+
+  }
+
+  render() {
+
+    if(!this.state.nameAvailable){
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+
+      </form>
+    );
+  }
+  else {
+    return (<div>Good Morning {this.state.value}</div>);
+  }
+
+
+
+
+  }
+}
+
+ export default NameForm;//name of class you want to export
